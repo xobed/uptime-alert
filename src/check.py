bac7154c5560cb21d2aa22a9648b0f88981039bb
Dusan Obert <obert.dusan@gmail.com>
2020-11-06 10:55:53 +0100

    Simple exception message Prevent sentry from creating duplicate issues

--- src/check.py
+++ src/check.py
@@ -1,10 +1,11 @@
 import os
+import time
+from datetime import datetime, timedelta
 from typing import List
 
 import requests
 import sentry_sdk
 from sentry_sdk import capture_exception
-from tenacity import stop_after_delay, retry, wait_fixed
 
 SENTRY_DSN = os.environ.get('SENTRY_DSN', default=None)
 if SENTRY_DSN is None:
@@ -27,15 +28,32 @@ if len(urls) == 0:
 retry_limit_seconds = int(os.environ.get('RETRY_LIMIT_SECONDS', default=60))
 
 
-@retry(stop=stop_after_delay(retry_limit_seconds), wait=wait_fixed(5))
 def check_url(url_to_check: str):
-    response = requests.get(url_to_check)
+    response = requests.get(url_to_check, timeout=10)
     response.raise_for_status()
-    print(f'{url} check passed')
+    print(f'{url_to_check} check passed')
+
+
+def check_with_retry(url_to_check: str):
+    timeout = datetime.now() + timedelta(seconds=retry_limit_seconds)
+    wait_between_retries = 5
+
+    while datetime.now() < timeout:
+        try:
+            check_url(url_to_check)
+            return
+        except Exception:
+            if datetime.now() < timeout:
+                time.sleep(wait_between_retries)
+
+    check_url(url_to_check)
+
 
 for url in urls:
     try:
-        check_url(url)
+        check_with_retry(url)
     except Exception as ex:
-        message = f'URL {url} is not responding: {ex}'
-        capture_exception(Exception(message, ex))
+        short_message = f'URL {url} is not responding'
+        message = f'{short_message}: {ex}'
+        print(message)
+        capture_exception(Exception(short_message))
